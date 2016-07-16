245b268f2fe314107c7483fd3f5048208f96eaef
kieaiaarh <kieaiaarh@gmail.com>
2016-07-16 22:13:50 +0900

    Fix spec

--- spec/support/elacticsearch.rb
+++ spec/support/elacticsearch.rb
@@ -1,6 +1,6 @@
 Spec.configure do |config|
   config.before(:all, :elasticsearch) do
-    Elasticsearch::Extensions::Test::Cluster.start port: 9250, nodes: 1) unless Elasticsearch::Extensions::Test::Cluster.running?(on: 9250)
+    Elasticsearch::Extensions::Test::Cluster.start port: 9250, nodes: 1 unless Elasticsearch::Extensions::Test::Cluster.running?(on: 9250)
   end
 
   config.after(:all, :elasticsearch) do
