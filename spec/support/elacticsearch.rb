d897ea99be78653b78325eec694d8d8fad53c0cd
kieaiaarh <kieaiaarh@gmail.com>
2016-07-17 22:58:09 +0900

    Try chaging test cluster initialize args

--- spec/support/elacticsearch.rb
+++ spec/support/elacticsearch.rb
@@ -4,7 +4,7 @@ RSpec.configure do |config|
 
   WebMock.allow_net_connect!
   config.before(:all, :elasticsearch) do
-    Elasticsearch::Extensions::Test::Cluster.start port: 9250 unless Elasticsearch::Extensions::Test::Cluster.running?(on: 9250)
+    Elasticsearch::Extensions::Test::Cluster.start port: 9250, network_host: 'localhost' unless Elasticsearch::Extensions::Test::Cluster.running?(on: 9250)
   end
 
   config.after(:all, :elasticsearch) do
