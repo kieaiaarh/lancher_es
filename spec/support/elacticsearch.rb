1a2c099d30e580217d6656f5143e0084e21867a8
kieaiaarh <kieaiaarh@gmail.com>
2016-07-17 23:08:08 +0900

    Reset test cluster command on ci and Webmock allow only localhost for test

--- spec/support/elacticsearch.rb
+++ spec/support/elacticsearch.rb
@@ -1,14 +1,12 @@
 require 'elasticsearch/extensions/test/cluster'
 
 RSpec.configure do |config|
-
-  WebMock.allow_net_connect!
   config.before(:all, :elasticsearch) do
-    Elasticsearch::Extensions::Test::Cluster.start port: 9250, network_host: 'localhost' unless Elasticsearch::Extensions::Test::Cluster.running?(on: 9250)
+    WebMock.disable_net_connect!(allow_localhost: true)
+    Elasticsearch::Extensions::Test::Cluster.start port: 9250 unless Elasticsearch::Extensions::Test::Cluster.running?(on: 9250)
   end
 
   config.after(:all, :elasticsearch) do
     Elasticsearch::Extensions::Test::Cluster.stop port: 9250 if Elasticsearch::Extensions::Test::Cluster.running?(on: 9250)
   end
-
 end
